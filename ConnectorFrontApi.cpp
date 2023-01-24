f720e7f19f3ac29f4d6e0768ef06213e7c8337c8
unknown <stevensoftware52@gmail.com>
2023-01-24 12:19:34 -0500

    Always log api messages

--- ConnectorFrontApi.cpp
+++ ConnectorFrontApi.cpp
@@ -7,14 +7,14 @@
 void ConnectorFrontApi::func_load_device(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_load_device %s", input.c_str());
+	blog(LOG_WARNING, "func_load_device %s", input.c_str());
 	ConnectorFrontApiHelper::createInterfaceObject(input, cd);
 }
 
 void ConnectorFrontApi::func_stop_receiver(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_stop_receiver %s", input.c_str());
+	blog(LOG_WARNING, "func_stop_receiver %s", input.c_str());
 	MediaSoupInterface::instance().getTransceiver()->StopReceiveTransport();
 
 	auto sourceInfo = static_cast<MediaSoupInterface::ObsSourceInfo *>(data);
@@ -25,14 +25,14 @@ void ConnectorFrontApi::func_stop_receiver(void *data, calldata_t *cd)
 void ConnectorFrontApi::func_stop_sender(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_stop_sender %s", input.c_str());
+	blog(LOG_WARNING, "func_stop_sender %s", input.c_str());
 	MediaSoupInterface::instance().getTransceiver()->StopSendTransport();
 }
 
 void ConnectorFrontApi::func_stop_consumer(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_stop_consumer %s", input.c_str());
+	blog(LOG_WARNING, "func_stop_consumer %s", input.c_str());
 	std::string destroyedId = MediaSoupInterface::instance().getTransceiver()->StopConsumerByProducerId(input);
 	auto sourceInfo = static_cast<MediaSoupInterface::ObsSourceInfo *>(data);
 
@@ -45,14 +45,14 @@ void ConnectorFrontApi::func_stop_consumer(void *data, calldata_t *cd)
 void ConnectorFrontApi::func_stop_producer(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_stop_producer %s", input.c_str());
+	blog(LOG_WARNING, "func_stop_producer %s", input.c_str());
 	MediaSoupInterface::instance().getTransceiver()->StopProducerById(input);
 }
 
 void ConnectorFrontApi::func_connect_result(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_connect_result %s", input.c_str());
+	blog(LOG_WARNING, "func_connect_result %s", input.c_str());
 
 	if (!MediaSoupInterface::instance().isThreadInProgress() || !MediaSoupInterface::instance().isConnectWaiting()) {
 		blog(LOG_ERROR, "%s func_connect_result but thread is not in good state", obs_module_description());
@@ -81,7 +81,7 @@ void ConnectorFrontApi::func_produce_result(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
 
-	blog(LOG_DEBUG, "func_produce_result %s", input.c_str());
+	blog(LOG_WARNING, "func_produce_result %s", input.c_str());
 
 	if (!MediaSoupInterface::instance().isThreadInProgress() || !MediaSoupInterface::instance().isProduceWaiting()) {
 		blog(LOG_ERROR, "%s func_produce_result but thread is not in good state", obs_module_description());
@@ -96,42 +96,42 @@ void ConnectorFrontApi::func_produce_result(void *data, calldata_t *cd)
 void ConnectorFrontApi::func_create_send_transport(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_create_send_transport %s", input.c_str());
+	blog(LOG_WARNING, "func_create_send_transport %s", input.c_str());
 	ConnectorFrontApiHelper::createSender(input, cd);
 }
 
 void ConnectorFrontApi::func_create_audio_producer(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_create_audio_producer %s", input.c_str());
+	blog(LOG_WARNING, "func_create_audio_producer %s", input.c_str());
 	ConnectorFrontApiHelper::createAudioProducerTrack(cd, input);
 }
 
 void ConnectorFrontApi::func_create_video_producer(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_create_video_producer %s", input.c_str());
+	blog(LOG_WARNING, "func_create_video_producer %s", input.c_str());
 	ConnectorFrontApiHelper::createVideoProducerTrack(cd, input);
 }
 
 void ConnectorFrontApi::func_create_receive_transport(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_create_receive_transport %s", input.c_str());
+	blog(LOG_WARNING, "func_create_receive_transport %s", input.c_str());
 	ConnectorFrontApiHelper::createReceiver(input, cd);
 }
 
 void ConnectorFrontApi::func_video_consumer_response(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_video_consumer_response %s", input.c_str());
+	blog(LOG_WARNING, "func_video_consumer_response %s", input.c_str());
 	ConnectorFrontApiHelper::createConsumer(*static_cast<MediaSoupInterface::ObsSourceInfo *>(data), input, "video", cd);
 }
 
 void ConnectorFrontApi::func_audio_consumer_response(void *data, calldata_t *cd)
 {
 	std::string input = calldata_string(cd, "input");
-	blog(LOG_DEBUG, "func_audio_consumer_response %s", input.c_str());
+	blog(LOG_WARNING, "func_audio_consumer_response %s", input.c_str());
 	ConnectorFrontApiHelper::createConsumer(*static_cast<MediaSoupInterface::ObsSourceInfo *>(data), input, "audio", cd);
 }
 
